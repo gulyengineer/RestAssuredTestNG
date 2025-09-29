66fca018389b0cdebd74be9e2d78c37014c15c1c
guly-engineer <94598500+guly-engineer@users.noreply.github.com>
2025-09-29 15:41:33 +0100

    New: Added Forgot Password API Test

--- src/test/java/com/example/base/AuthService.java
+++ src/test/java/com/example/base/AuthService.java
@@ -4,6 +4,8 @@ import com.example.models.request.LoginRequest;
 import com.example.models.request.SignupRequest;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+
 public class AuthService extends BaseService {
     private static final String BASE_PATH = "/api/auth/";
 
@@ -13,5 +15,11 @@ public class AuthService extends BaseService {
 
     public Response signup(SignupRequest payload) {
         return postRequest(payload, BASE_PATH + "signup");
+    }
+
+    public Response forgotPassword(String emailAddress) {
+        HashMap<String, String> payload = new HashMap<>();
+        payload.put("email", emailAddress);
+        return postRequest(payload, BASE_PATH + "forgot-password");
     }
 }
--- src/test/java/com/example/tests/LoginAPITest.java
+++ src/test/java/com/example/tests/LoginAPITest.java
@@ -3,30 +3,16 @@ package com.example.tests;
 import com.example.base.AuthService;
 import com.example.model.response.LoginResponse;
 import com.example.models.request.LoginRequest;
+import com.example.utils.CommonDataProviders;
 import io.restassured.response.Response;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 
 public class LoginAPITest {
-    @DataProvider(name = "credentialsFromEnv")
-    public Object[][] credentialsFromEnv() {
-        String username = System.getenv("USERNAME");
-        String password = System.getenv("PASSWORD");
-        String expectedEmail = System.getenv("EXPECTED_EMAIL");
-
-        // If you prefer system properties (mvn -D...), use System.getProperty("USERNAME")
-        if (username == null || password == null || expectedEmail == null) {
-            throw new IllegalStateException("Missing environment variables USERNAME, PASSWORD or EXPECTED_EMAIL");
-        }
-
-        return new Object[][]{{username, password, expectedEmail}};
-    }
-
-    @Test(description = "Test login API", dataProvider = "credentialsFromEnv")
-    public void login(String username, String password, String expectedEmail) {
+    @Test(description = "Test login API", dataProvider = "credentialsFromEnv", dataProviderClass = CommonDataProviders.class)
+    public void login(String username, String password, String email) {
         AuthService authService = new AuthService();
         LoginRequest loginRequest = new LoginRequest(username, password);
         Response response = authService.login(loginRequest);
@@ -34,7 +20,7 @@ public class LoginAPITest {
         System.out.println(response.asPrettyString());
         System.out.println(loginResponse.getToken());
         assertNotNull(loginResponse.getToken());
-        assertEquals(loginResponse.getEmail(), expectedEmail);
+        assertEquals(loginResponse.getEmail(), email);
         assertEquals(loginResponse.getId(), 2891);
     }
 }
